1ebbe3fb7a737b1ef130955230a81247c72e010f
joao-AR <jp.alvesrodrigues02@gmail.com>
2023-05-01 20:09:32 -0300

    campo Minado

---
+++ CampoMinado/App.js
@@ -0,0 +1,57 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params';
+import Field from './src/components/Field';
+import MineField from './src/components/MineField';
+import {createMineBoard} from './src/gameLogic'
+import { Component } from 'react';
+
+export default class App extends Component {
+  
+  constructor(props){
+    super(props)
+    this.state = this.createState
+  }
+    minesAmount = () =>{
+    const cols = params.getColumnAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState =  () =>{
+    const cols = params.getColumnAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board : createMineBoard(rows,cols, this.minesAmount())
+    }
+  }
+  
+  render(){
+    return (
+      <View style={styles.container}>
+          <Text> Iniciando o campo minado2 </Text>
+          <Text>Tamanho da grade {params.getRowsAmount()} X {params.getColumnAmount()}</Text>
+          <Field />
+          <Field  opened />
+          <Field  opened nearMines={1}/>
+          <Field  opened nearMines={2}/>
+          <Field  opened nearMines={5}/>
+          <Field  opened nearMines={6}/>
+          <Field  mined/>
+          <Field  opened mined/>
+          <Field  opened mined exploded/>
+          <Field  flagged/>
+          
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
